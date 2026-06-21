db42d5e7c76fcd556bd4fbc42bf7ae8b8fa1079b
Bright Joel D <125977775+JoelLooker@users.noreply.github.com>
2026-06-21 23:59:35 +0530

    Replace write with markdown for scripture display

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
+st.markdown("""
+Joel 2:12 New Revised Standard Version, Anglicised  
 
-st.write('Joel 2:12 New Revised Standard Version, Anglicised\n'
-         '12 Yet even now, says the Lord,\n'
-         'return to me with all your heart,\n'
-         'with fasting, with weeping, and with mourning')
+12 Yet even now, says the Lord,  
+return to me with all your heart,  
+with fasting, with weeping, and with mourning
+""")
