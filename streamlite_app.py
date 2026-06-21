4d8720882a7946a1659a7886d59b57eb0351cd0e
Bright Joel D <125977775+JoelLooker@users.noreply.github.com>
2026-06-21 23:56:37 +0530

    Refactor scripture output for better formatting

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
-st.write('Joel 2:12
-New Revised Standard Version, Anglicised
-12 Yet even now, says the Lord,
-    return to me with all your heart,
-with fasting, with weeping, and with mourning
-')
+
+st.write('Joel 2:12 New Revised Standard Version, Anglicised\n'
+         '12 Yet even now, says the Lord,\n'
+         'return to me with all your heart,\n'
+         'with fasting, with weeping, and with mourning')
