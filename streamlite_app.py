00cc444a07ac7efbd7d767e804218f7314c4db19
Bright Joel D <125977775+JoelLooker@users.noreply.github.com>
2026-06-21 23:54:42 +0530

    Update greeting message to a scripture quote

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.write('Hello BI community')
+st.write('Joel 2:12
+New Revised Standard Version, Anglicised
+12 Yet even now, says the Lord,
+    return to me with all your heart,
+with fasting, with weeping, and with mourning
+')
